a35ddf232baa36ec7245af0fce080e42bf94ce3e
PetrosKatiforis <42544934+PetrosKatiforis@users.noreply.github.com>
2023-01-01 13:07:45 +0200

    Removed pointless comments and updated linked list loop

--- src/dictionary_parser.c
+++ src/dictionary_parser.c
@@ -5,37 +5,34 @@
 #include "dictionary_parser.h"
 
 /*
- * Reads the file and generates the linked list one level at a time
- * The data is extracted using an fscanf format
+ * Reads the file and generates the linked list
+ * The data is extracted using fscanf
  */
 dictionary_item_t* parse(const char* filename)
 {
     // Opening the dictionary file
     FILE* file = fopen(filename, "r");
 
-    if (file == NULL) 
+    if (!file) 
         return NULL;
 
-    // Holding a reference to the previously parsed item
+    // Creating a linked list structure with pointers of malloced dictionary_item_t
     dictionary_item_t* previous_item = NULL;
 
-    // Reading each line one by one until there's no more input
     while (true)
     {
         dictionary_item_t* item = malloc(sizeof(dictionary_item_t));
         item->next = previous_item;
 
-        // Collecting data from file and assigning to struct
+        // Collecting data from file and assigning to so that they can be processed later
         int result = fscanf(file, "%s = %s", item->original, item->translation);
 
         // Checking if it has reached the end of the file
-        if (result == EOF)
-            break;
+        if (result == EOF) break;
 
         previous_item = item;
     }
 
-    // Closing up the file
     fclose(file);
 
     return previous_item;
@@ -46,15 +43,9 @@ dictionary_item_t* parse(const char* filename)
  */
 void traverse_dictionary(dictionary_item_t* root, dictionary_item_handler_t handler)
 {
-    // Temporary and auxiliary pointer
-    dictionary_item_t* current_item = root;
-    
-    while (current_item != NULL)
+    for (dictionary_item_t* current_item = root; current_item != NULL; current_item = current_item->next)
     {
-        // Calling handler function
         handler(current_item);
-
-        current_item = current_item->next;
     }
 }
 
